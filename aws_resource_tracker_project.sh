6b8f5bf9d4ff0b05bcb39bb80b307a8158583bdd
Wale Johnson <citi.standards@gmail.com>
2023-08-22 07:02:39 +0000

    AWS Resource Trackers

---
+++ aws_resource_tracker_project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+<<intro
+Author: Wale J
+Date: 09 Aug 2023
+Project: AWS resource tracker
+intro
+
+
+<<Resource
+Resources to be tracked:
+aws s3 bucket
+aws ec2 instance
+lambda function
+IAM user
+Resource
+
+echo "PRINT S3 BUCKETS"
+aws s3 ls
+
+echo "PRINT EC2 INSTANCES"
+aws ec2 describe-instances
+
+echo "PRINT LAMBDA FUNCTION"
+aws lambda list-functions
+
+echo "PRINT IAM USERS"
+aws iam list-users
---
+++ aws_resource_tracker_project2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+<<intro
+Author: Wale Johnson
+Date: 09 Aug 2023
+Project: AWS resource tracker
+intro
+
+set -x
+
+<<Resource
+Resources to be tracked:
+aws s3 bucket
+aws ec2 instance
+lambda function
+IAM user
+Resource
+
+echo "PRINT S3 BUCKETS"
+aws s3 ls
+
+echo "PRINT EC2 INSTANCES"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+echo "PRINT LAMBDA FUNCTION"
+aws lambda list-functions
+
+echo "PRINT IAM USERS"
+aws iam list-users
